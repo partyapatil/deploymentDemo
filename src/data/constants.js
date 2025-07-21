64c27992fada80d2e02af08e54626e12ec011396
Prathamesh Patil <146924439+partyapatil@users.noreply.github.com>
2025-07-22 00:25:57 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "Programmer",
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    "want to see live deploymentI am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/partyapatil/new.git",
   resume:
     "https://drive.google.com/file/d/1MKJv54lBWS6lU81ET9zurzqziNWXqa5Y/view?usp=sharing",
